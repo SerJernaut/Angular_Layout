918125442bfac8ca3e8c76f4728be7bfe3658e6c
SerJernaut <serjernaut@yandex.ru>
2020-08-29 11:08:16 +0300

    Create Header Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
+import {HeaderComponent} from './header/header.component';
 
-import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   imports: [
@@ -18,10 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent
+    HeaderComponent
   ],
   bootstrap: [ AppComponent ]
 })
